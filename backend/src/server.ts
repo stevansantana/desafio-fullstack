e4de92ab4c72dc99b3dac27ea4779baec7318e8e
Stevan <stevansantanas@gmail.com>
2026-04-24 07:53:49 -0300

    Tarefa: criar endpoint de usuário com o Prisma

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,22 @@
 import express from "express";
+import { prisma } from "./lib/prisma";
 
 const app = express();
 
 app.use(express.json());
+
+app.post("/users", async (req, res) => {
+  const { nome, email } = req.body;
+
+  const user = await prisma.user.create({
+    data: {
+      nome,
+      email,
+    },
+  });
+
+  res.status(201).json(user);
+});
 
 app.listen(3001, () => {
   console.log("Servidor rodando na porta 3001");
