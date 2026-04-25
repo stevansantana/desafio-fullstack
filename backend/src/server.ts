d3a6761f7ebc83396259a1929ea18173f0b3e9a0
Stevan <stevansantanas@gmail.com>
2026-04-25 10:15:48 -0300

    fix: habilitar cors para integração entre frontend e backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import userRoutes from "./routes/childRoutes";
 import "dotenv/config";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
 app.use(userRoutes);
 
 app.listen(3001, () => {
